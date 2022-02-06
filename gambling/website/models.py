e41c7db18c8bfbab20a0b4d69711e74b6edef948
Barry Carter <contactcarter@hotmail.com>
2022-02-06 15:34:45 +0000

    first login version

--- gambling/website/models.py
+++ gambling/website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ItemInfo(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+
+class Game(models.Model):
+    status = models.CharField(max_length=100)  
+
+class GamePlayer(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+class PlayerInventory(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)    
+    itemInfo = models.ForeignKey(ItemInfo, on_delete=models.CASCADE)
--- gambling/website/views.py
+++ gambling/website/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from urllib import response
+from django.shortcuts import render, redirect
 # Create your views here.
+from django.http import HttpResponse
+from .models import Game
+
+def index(request):
+    latestGames = Game.objects.order_by('-pk')[:5]
+    context = {'latestGames':latestGames}
+    return render(request,"website\games_index.html",context)
+
+def login(request):    
+    return render(request,"website\login.html")
+
+def loginprocess(request):    
+    if not 'username' in request.POST:
+        return redirect('/login')
+
+    username = request.POST['username']
+    
+    resp = index(request)
+    resp.set_cookie('username', username)
+
+    return resp
+
+def newgame(request):
+
+    if not 'username' in request.COOKIES:
+        return redirect('/login')
+
+    latestGames = Game()
+    latestGames.status='Open'
+    latestGames.save()
+
+    return redirect('/')
